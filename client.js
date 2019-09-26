93cbe206664dc6914c19198aedb7d3780b767c5d
James <jrjefferson830@gmail.com>
2019-09-25 21:51:04 -0500

    revised version

---
+++ client.js
@@ -0,0 +1,85 @@
+let allEmployees = [];
+
+$(document).ready(onReady);
+
+function onReady() {
+    console.log('in onReady');
+    // render a table with the `` data
+    render();
+    $('.js-btn-employee').on(`click`, onClickAddEmployee);
+    $('.js-btn-delete').on(`click`, deleteEmployee);
+}
+
+function onAddInfo(event){
+    event.preventDefault();
+    const  formArray= $(this).serializeArray();
+    console.table(formArray);
+    const profileEmployee = {};
+    for(let field of formArray){
+        const objectKey = field.name;
+        const objectValue = field.value
+
+        profileEmployee[objectKey] = objectValue;
+    }
+    console.log(profileEmployee);
+
+    render();
+}
+    function deleteEmployee() {
+        $(this).parent().remove();
+    }
+
+function onClickAddEmployee() {
+    const newEmployee = {
+        firstname: $('.js-input-firstname').val(),
+        lastname: $('.js-input-lastname').val(),
+        idnumber: parseInt($('.js-input-idnumber').val()),
+        jobtitle: $('.js-input-jobtitle').val(),
+        annualsalary: parseInt($('.js-input-annualsalary').val())
+    }
+    console.log(newEmployee);
+    allEmployees.push(newEmployee);
+    render();
+
+    $('.js-input-firstname').val(''),
+    $('.js-input-lastname').val(''),
+    $('.js-input-idnumber').val(''),
+    $('.js-input-jobtitle').val(''),
+    $('.js-input-annualsalary').val('')
+}
+function render(){
+    console.log('render');
+    const tableElement = $('.js-table-data')
+
+    
+        tableElement.empty();
+        let totalMonthly =0
+        for (let i = 0; i < allEmployees.length; i++) {
+            const employee = allEmployees[i];
+    
+            tableElement.append(`<tr id='row'>
+                <td>${employee.firstname}</td>
+                <td>${employee.lastname}</td>
+                <td>${employee.idnumber}</td>
+                <td>${employee.jobtitle}</td>
+                <td>${employee.annualsalary}</td>
+            </tr>`);
+        
+        totalMonthly += parseInt(allEmployees.annualsalary);
+        }
+    
+
+        let salaryTotal = $('totalSummary')
+        salaryTotal.empty();
+        $('#totalSalaryOut').append( totalMonthly );
+
+        if(totalMonthly > 200000){
+            $('.sumTotal').css('background-color','red');
+            $('.sumTotal').css('color', 'yellow');
+        }
+   
+    }
+    
+
+
+
